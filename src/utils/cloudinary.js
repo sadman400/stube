48f96ad0c18cee06dcc63c30716b03835be3c389
sadman <sadmanhossain400@gmail.com>
2024-01-28 04:11:33 +0600

    update

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -19,6 +19,7 @@ const uploadOnCloudinary = async (localFilePath) => {
         })
         // file has been uploaded successfully 
         console.log("file is uploaded on cloudinary", response.url);
+        fs.unlinkSync(localFilePath);
         return response;
     } catch (error) {
         fs.unlinkSync(localFilePath) // local file deleted if upload faild
